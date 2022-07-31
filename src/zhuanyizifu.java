09fcf17645518655ad21ea01152c106c3485b31a
懒大王想白嫖 <110296351+akale6@users.noreply.github.com>
2022-07-31 21:59:13 +0800

    Add files via upload

---
+++ src/zhuanyizifu.java
@@ -0,0 +1,22 @@
+/**
+ * @author lcl
+ *///文档注释的使用
+public class zhuanyizifu {
+    public static void main(String[]args){
+   //     \t ：一个制表位，实现对齐的功能
+        System.out.println("北京\t西安\t上海");
+//  \n ：换行符
+        System.out.println("deamxiya\nakli");
+// \ ：一个\
+        System.out.println("hello\\china");//输出为hello\china
+// \ " :一个"
+        System.out.println("我说:\"我爱你\"");//输出为"我爱你"
+        System.out.println("我说:\'我爱你\'");//输出为'我爱你'
+// \' ：一个' \r :一个回车 System.out.println("韩顺平教育\r 北京
+        System.out.println("德玛西亚\r\n万众一心");/*多行注释表示
+        哈哈哈哈*/
+        System.out.println("书名\t作者 \t价格\t销量\n三国\t罗贯中\t120\t1000");//实战
+        System.out.println("lcl\tman\t");
+//jdk = jre + java开发工具 jre = jvm + 核心类库
+    }
+}
